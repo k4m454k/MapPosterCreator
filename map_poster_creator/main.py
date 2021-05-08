124f888038e5be1b392e5567de38b54ca48114c9
Vadim Apenko <k4m454k@gmail.com>
2021-05-09 00:39:45 +0300

    Final choice for comment =)

--- map_poster_creator/main.py
+++ map_poster_creator/main.py
@@ -1,7 +1,6 @@
 from typing import Tuple, List, Dict, Union
 
 import fiona  # noqa For Apple Silicon (M1) CPU
-# it needs! Else Exception!
 from geopandas import GeoDataFrame
 from shapely.geometry import Polygon
 
